a740330d6afd6399a12baf5698403fd28f127e6d
Mickaël Wolff <m@lupusmic.org>
2019-08-29 15:12:22 +0200

    Test reversing the list

--- list_test.go
+++ list_test.go
@@ -27,3 +27,28 @@ func TestList(t *testing.T) {
 	}
 
 }
+
+func TestReverse(t *testing.T) {
+	cases := []struct {
+		in []string
+		want string
+	}{
+		{ []string{}, "List []" },
+		{ []string{"first"}, "List ['first']" },
+		{ []string{"first", "second"}, "List ['first','second']" },
+		{ []string{"first", "second", "third"}, "List ['first','second','third']" },
+	}
+
+	for _, esac := range cases {
+		var list = List{nil}
+		list.AddMany(esac.in...)
+		list.Reverse()
+
+		var got = list.String()
+
+		if got != esac.want {
+			t.Errorf("Reverse(%q) == %q, want %q", esac.in, got, esac.want)
+		}
+	}
+
+}
